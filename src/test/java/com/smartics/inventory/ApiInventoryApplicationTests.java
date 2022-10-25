b6b6d964514ad0bc5039d8f55ed8cf767d2c4f68
Jose Wilmer Manzano <josewilmerm1@hotmail.com>
2022-10-24 20:49:42 -0500

    feat: Create index.html

---
+++ src/test/java/com/smartics/inventory/ApiInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartics.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
